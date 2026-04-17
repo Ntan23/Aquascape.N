85346bebc90c94471bec6d166b996c74613fa8c4
NT <ntan231002@gmail.com>
2026-04-17 22:28:23 +0700

    Add Particle Effect

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -5,6 +5,10 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public ConfigManager configManager;
     public PlayerInput playerInput;
+
+    [Header("Particle Effects")]
+    [SerializeField] private ParticleSystem bubbleParticles;
+    [SerializeField] private ParticleSystem clickParticlesEffect;
     
     void Awake()
     {
@@ -20,9 +24,14 @@ public class GameManager : MonoBehaviour
 
         configManager = new ConfigManager();
         configManager.Init();
+    }
 
+    void Start()
+    {
         playerInput = new PlayerInput();
         playerInput.Init(this);
+
+        SetupBubbles();
     }
 
     void Update()
@@ -31,5 +40,23 @@ public class GameManager : MonoBehaviour
         {
             playerInput.DoUpdate();
         }
+    }
+
+    public void SetupBubbles()
+    {
+        float lowerBound = -configManager.currentConfig.spawnSetting.spawnAreaHeight / 2.0f;
+
+        bubbleParticles.transform.position = new Vector3(0, lowerBound, 0);
+        var particlesShape = bubbleParticles.shape;
+        particlesShape.radius = configManager.currentConfig.spawnSetting.spawnAreaWidth / 2.0f;
+
+        bubbleParticles.gameObject.SetActive(true);
+    }
+
+    public void PlayClickParticleEffect(Vector2 position)
+    {
+        clickParticlesEffect.Stop();
+        clickParticlesEffect.transform.position = position;
+        clickParticlesEffect.Play();
     }
 }
